1d49abb79f24438ea059cfdae46c8478a3d4f3de
Aleksei Evstafev <a.a.evstafyev@gmail.com>
2019-10-16 16:49:16 +0300

    Version 2.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# pnpFinder
+
+- Version: 2.0
+- Author: Alex Evstafiev
+- Creation Date:  01.08.2019
+- Purpose/Change: Inventory management
+- PS Version: 5.0 
+
+## Usage
+    
+The script is using for quick view PNP ID of connected USB device. Received PNP ID can be used for details inventory cards. Output looks like cutted devcon.exe -find @usb\*
+
+## Setup
+
+The script runs out of console. 
+
+Windows Management Instrumentation (winmgmt) service should be run!
+
+## Output
+
+![Screenshot](/png/pnpFinder-console-output-example.PNG) 
+
+## Scope
+
+Any device with USB interface.
---
+++ pnpFinder.ps1
@@ -0,0 +1,96 @@
+﻿function Get-usbCounter {
+
+    $usbDevciesCounter = @()
+
+    gwmi win32_usbdevice | % {$usbDevciesCounter += 1}
+
+    $activeUsbDevices = @($usbDevciesCounter).length
+
+    return $activeUsbDevices
+
+}
+
+function Get-pnpId {
+
+    $pnpId = @()
+
+    gwmi win32_usbdevice | 
+        select -exp PNPDeviceID | 
+        sls 'PID_.*?(?<=)[A-Z,0-9]\\(.*?)$' -AllMatches | 
+        % {
+            $pnpId += $_.Matches.groups[1]
+        }
+
+    return $pnpId
+
+}
+
+Clear-Variable -Name promptExit
+
+While ("n" -notcontains $promptExit) {
+
+    $bPnpId = Get-pnpId
+
+    $bDevices = Get-usbCounter
+
+    Write "---------------------"
+
+    Write-Host "Active USB devices: $bDevices" -ForegroundColor Yellow
+
+    Start-sleep 2
+
+    Write-host "Connect target USB device!" -ForegroundColor Yellow
+
+    Start-sleep 2
+    
+    $checkDevice = Read-Host "Target USB device connected? (y/n)"
+
+    while("y" -notcontains $checkDevice) {
+
+        $checkDevice = Read-Host "Target USB devices connected? (y/n)"
+
+    }
+
+    $uDevices = Get-usbCounter
+
+    if ($bDevices -eq $uDevices) {
+
+        Write-Host "Device not connected! Please try again..." -ForegroundColor Red
+
+    }
+
+    elseif ($bDevices -gt $uDevices) {
+
+        $status = 'Disconnected'
+    
+        Write-Host "Device disconnected!" -ForegroundColor Red
+
+    }
+
+    else { 
+
+        $status = 'Connected'
+
+    }
+
+    $uPnpId = Get-pnpId
+
+    Write-Host "Active USB devices: $uDevices" -ForegroundColor Green
+    
+    $c = (Compare-Object -ReferenceObject $bPnpId -DifferenceObject $uPnpId).InputObject.Value
+
+    $i = 1
+
+    foreach ($dev in $c) {
+
+        Write-Host "$status PNP ID #$i : $dev" -ForegroundColor Green
+
+        $i++
+
+    }
+
+    Write "---------------------"
+
+    $promptExit = Read-Host "Continue? (y/n)"
+
+}
